d5fac015cc27c7bbfc6c85f5b8d443bdb066991e
MarieliScheffer <mariwassen13@gmail.com>
2024-12-12 01:08:13 +0000

    fix: inclusão de login e dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
-import Principal from "./principal"
+import { LoginForm } from "@/components/login-form";
 export default function Home() {
   return (
-      <h2>teste</h2>
+      <div className="flex h-screen w-full items-center justify-center px-4">
+            <LoginForm />
+          </div>
   );
 }
